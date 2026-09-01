56bc2002c7bcff6e3cbce98b6d285a424178a849
aamandaps <aamandaps01@gmail.com>
2026-08-31 23:26:32 -0300

    +8 exercicios

--- exercicios_loop/src/ex01.java
+++ exercicios_loop/src/ex01.java
@@ -1,2 +1,21 @@
+import javax.swing.JOptionPane;
+
 public class ex01 {
+
+    /* Calcule e mostre o quadrado dos números entre 10 e 150. */
+
+    public static void main(String[] args) {
+
+        int num, quad;
+
+        for (num = 10;num<=150;num++)
+        {
+            quad = num*num;
+
+            JOptionPane.showMessageDialog(null , "O quadrado de "+num+" é igual à "+quad);
+
+        } // Fim Loop
+
+    }
+
 }
